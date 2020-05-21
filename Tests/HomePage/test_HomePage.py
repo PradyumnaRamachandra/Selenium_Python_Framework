04263f3e2e175075798822c4e4afe685af707ca1
Pradyumna R <prady07@gmail.com>
2020-05-21 22:38:21 +0530

    Updated Framework

--- Tests/HomePage/test_HomePage.py
+++ Tests/HomePage/test_HomePage.py
@@ -32,7 +32,7 @@ class TestHomePage(BaseClass):
         log.info(text)
 
         self.takeScreenshot()
-        assert "Success" in text,"Text is not present"
+        assert "Success123" in text,"Text is not present"
         self.driver.refresh()
         time.sleep(1)
         log.info("Test Completed")
